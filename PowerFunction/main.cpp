d847f83b498757929e737988bee7882dedc5adc1
陈宇峰 <yufengcode@gmail.com>
2016-09-30 10:35:23 +0800

    for

--- PowerFunction/main.cpp
+++ PowerFunction/main.cpp
@@ -8,8 +8,33 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+/**
+ *  实现power函数
+ *
+ double power(double x ,int n);
+ *
+ */
+
+double power1(double x ,int n);
+
+int main(int argc, const char * argv[])
+{
+    cout << power1(2.0, -2) << endl;
+
     return 0;
+}
+
+// 最简单的方式，复杂度为n
+double power1(double x ,int n)
+{
+    double result = 1.0;
+    int absN = abs(n);
+    for (int i = 0; i < absN; i++)
+    {
+        result *= x;
+    }
+
+    return n >= 0 ? result : 1/result;
 }
